2937d4e109cecb68234fd45e1f79b66ae046fba4
Sulman-P <odhiambosulman3@gmail.com>
2026-03-11 08:48:01 +0300

    Update admin.js
    Signed-off-by: Sulman-P <odhiambosulman3@gmail.com>

--- admin.js
+++ admin.js
@@ -37,3 +37,72 @@ loadExams()
 }
 
 }
+async function addQuestion(){
+
+const examId = document.getElementById("examId").value
+
+const question = document.getElementById("questionText").value
+
+const a = document.getElementById("optionA").value
+const b = document.getElementById("optionB").value
+const c = document.getElementById("optionC").value
+const d = document.getElementById("optionD").value
+
+const correct = document.getElementById("correctAnswer").value
+
+const { error } = await supabaseClient
+.from("questions")
+.insert([
+{
+exam_id: examId,
+question: question,
+option_a: a,
+option_b: b,
+option_c: c,
+option_d: d,
+correct: correct
+}
+])
+
+if(error){
+
+alert(error.message)
+
+}else{
+
+alert("Question Added")
+
+}
+
+}
+async function loadExams(){
+
+let { data } = await supabaseClient
+.from("exams")
+.select("*")
+
+let html = ""
+
+data.forEach(exam => {
+
+html += `
+<div class="exam-card">
+
+<h3>${exam.title}</h3>
+
+<p>${exam.subject}</p>
+
+<p>Time: ${exam.time_limit} min</p>
+
+<p>ID: ${exam.id}</p>
+
+</div>
+`
+
+})
+
+document.getElementById("examList").innerHTML = html
+
+}
+
+loadExams()
